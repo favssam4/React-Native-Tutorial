6f1341633225a7c9cbf07849e13f029b75675a80
Jon Ray Favorito <jfavorito@gbox.adnu.edu.ph>
2019-08-26 10:30:14 +0800

    Splitting and Styling SharePlace

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
 
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 class PlaceInput extends Component {
   state = {
     placeName: ""
@@ -12,43 +12,17 @@ class PlaceInput extends Component {
     });
   };
 
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
 
-    this.props.onPlaceAdded(this.state.placeName);
-  };
-
-    render() {
-        return (
-            <View style={styles.inputContainer}>
-                <TextInput  style={styles.placeInput}
-                            value={this.state.placeName}
-                            placeHolder="An awesome place"
-                            onChangeText={this.placeNameChangedHandler}>
-                </TextInput>
-                <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler}></Button>
-            </View>
-        );
-    }
+  render() {
+    return (
+        <DefaultInput placeholder="Place Name" 
+                      value={this.state.placeName} 
+                      onChangeText={this.placeNameChangedHandler}
+        />
+    );
+  }
 
     
 }
-
-const styles = StyleSheet.create({
-    inputContainer: {
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    placeInput: {
-        width: "70%"
-    },
-    placeButton: {
-        width: "30%"
-    }
-});
 
 export default PlaceInput;
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Button, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import { View, TouchableOpacity, Button, StyleSheet, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { addPlace } from '../../store/actions/index';
 import { Ionicons } from '@expo/vector-icons';
 
-import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import PickImage from '../../components/PickImage/PickImage';
+import PickLocation from '../../components/PickLocation/PickLocation';
+
 import MainText from '../../components/UI/MainText/MainText';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
-
-import prairie from '../../assets/prairie.jpg';
 
 class SharePlaceScreen extends Component {
     static navigationOptions = ({navigation}) => {
@@ -34,17 +35,9 @@ class SharePlaceScreen extends Component {
                     <MainText>
                         <HeadingText>Share a Place with us!</HeadingText>
                     </MainText> 
-                    <View style={styles.placeHolder}>
-                        <Image source={prairie} style={styles.imageHolder} />
-                    </View>
-                    <View style={styles.button}>
-                        <Button title="Pick Image" />
-                    </View>
-                    <View style={styles.placeHolder}><Text>Map</Text></View>
-                    <View style={styles.button}>
-                        <Button title="Locate Me" />
-                    </View>
-                    <DefaultInput placeholder="Place Name" />
+                    <PickImage />
+                    <PickLocation />
+                    <PlaceInput />
                     <View style={styles.button}>
                         <Button title="Share the Place!" />
                     </View>
